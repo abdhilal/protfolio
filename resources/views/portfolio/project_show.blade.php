75de97d7406829370a356ce3bfe474e552a5af01
Abdulrahman hilal <abdrahmanmhran3@gmail.com>
2025-06-05 01:28:31 +0300

    the end maybe

--- resources/views/portfolio/project_show.blade.php
+++ resources/views/portfolio/project_show.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ERP System - Project Details - Abdulrahman Hilal</title>
+    <title>{{$project->title}} - Abdulrahman Hilal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href={{ asset('fontawesome/css/all.min.css') }}>
     <link rel="stylesheet" href={{ asset('css/style.css') }}>
