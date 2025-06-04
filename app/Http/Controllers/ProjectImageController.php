9d75c9a4cdd69f8b4cc0502c90ef914050844c85
Abdulrahman hilal <abdrahmanmhran3@gmail.com>
2025-06-04 15:46:08 +0300

    the end maybe

--- app/Http/Controllers/ProjectImageController.php
+++ app/Http/Controllers/ProjectImageController.php
@@ -12,42 +12,6 @@ class ProjectImageController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(ProjectImage $projectImage)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(ProjectImage $projectImage)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
