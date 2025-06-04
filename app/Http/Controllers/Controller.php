0455fdd4842d0785a47943c812701d2107d5e598
Abdulrahman hilal <abdrahmanmhran3@gmail.com>
2025-06-04 16:17:28 +0300

    the end maybe

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    // This is a base controller class that can be extended by other controllers.
-    // It can contain shared methods or properties for all controllers in the application.
-    // Currently, it does not have any specific functionality.
+   
 }
